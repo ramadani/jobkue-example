62d7f387ac0cecef389f845daf2da2433b5a4975
ramadani <email.ramadani@gmail.com>
2020-03-14 21:50:11 +0700

    using worker pool

--- main.go
+++ main.go
@@ -1,59 +1,55 @@
 package main
 
 import (
-	"log"
-	"sync"
+	"fmt"
 	"time"
 )
-
-var wg sync.WaitGroup
-
-func main() {
-
-	jobChanMap := make(map[string]chan Job)
-	jobChanMap["test"] = make(chan Job, 5)
-	jobChanMap["test2"] = make(chan Job, 5)
-
-	for k, jobChan := range jobChanMap {
-		wg.Add(1)
-		go worker(jobChan)
-
-		for i := 0; i < 10; i++ {
-			if !TryEnqueue(Job{ID: i + 1, Key: k}, jobChan) {
-				log.Println("max caps")
-			}
-		}
-
-		close(jobChan)
-
-		wg.Wait()
-	}
-
-	//jobChan := jobChanMap["test"]
-
-	//time.Sleep(20 * time.Second)
-}
 
 type Job struct {
 	ID  int
 	Key string
 }
 
-func worker(jobChan <-chan Job) {
-	defer wg.Done()
+type Result struct {
+	ID  int
+	Key string
+}
 
-	for job := range jobChan {
-		log.Println("start job", job.ID, "key", job.Key)
-		time.Sleep(100 * time.Millisecond)
-		log.Println("end job", job.ID, "key", job.Key)
+func main() {
+	numJobs := 5
+	jobChanMap := make(map[string]chan Job)
+	resChanMap := make(map[string]chan Result)
+
+	jobChanMap["test"] = make(chan Job, numJobs)
+	jobChanMap["test2"] = make(chan Job, numJobs)
+	resChanMap["test"] = make(chan Result, numJobs)
+	resChanMap["test2"] = make(chan Result, numJobs)
+
+	for k, jobChan := range jobChanMap {
+		resChan := resChanMap[k]
+
+		go worker(jobChan, resChan)
+
+		for i := 1; i <= numJobs; i++ {
+			jobChan <- Job{
+				ID:  i,
+				Key: k,
+			}
+		}
+		close(jobChan)
+
+		for i := 1; i <= numJobs; i++ {
+			r := <-resChan
+			fmt.Println("worker", r.Key, "result", r.ID)
+		}
 	}
 }
 
-func TryEnqueue(job Job, jobChan chan<- Job) bool {
-	select {
-	case jobChan <- job:
-		return true
-	default:
-		return false
+func worker(jobChan <-chan Job, resChan chan<- Result) {
+	for job := range jobChan {
+		fmt.Println("worker", job.Key, "started job", job.ID)
+		time.Sleep(50 * time.Millisecond)
+		fmt.Println("worker", job.Key, "finished job", job.ID)
+		resChan <- Result{ID: job.ID, Key: job.Key}
 	}
 }
